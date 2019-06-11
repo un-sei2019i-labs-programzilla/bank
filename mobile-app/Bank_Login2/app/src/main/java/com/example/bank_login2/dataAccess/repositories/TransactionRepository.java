e7c0bb40673e2712714e2ddb385c7c26fff3615a
Nico <nsochoaga@unal.edu.co>
2019-06-11 03:02:50 -0500

    login

--- mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/dataAccess/repositories/TransactionRepository.java
+++ mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/dataAccess/repositories/TransactionRepository.java
@@ -1,4 +1,5 @@
 package com.example.bank_login2.dataAccess.repositories;
 
 public class TransactionRepository {
+
 }
--- mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/dataAccess/repositories/UserRepository.java
+++ mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/dataAccess/repositories/UserRepository.java
@@ -46,17 +46,43 @@ public class UserRepository {
         database.close();
     }
 
-    public void getUSerByID(Context context, int id) {
-        Database db = new Database(context, "BD1", null, 1);
-        SQLiteDatabase database = db.getReadableDatabase();
+    public String getUSerByID(Context context, int id) {
+
+        Database db = new Database(context, "DB1", null, 1);
+        SQLiteDatabase database = db.getWritableDatabase();
         Cursor cursor = database.query("USER", new String[]{"NAME", "ACCOUNT_NUMBER", "PASSWORD"},
                 "ID = ?", new String [] {Integer.toString(id)}, null, null, null);
         if (cursor.moveToFirst()){
 
+
             Toast.makeText(context, (cursor.getString(0) + " " + cursor.getString(1)
                 + " " + cursor.getString(2)), Toast.LENGTH_LONG).show();
+
+            return cursor.getString(0);
         }
         database.close();
+
+        return null;
     }
 
+    public boolean comp(Context context, String nom, String pass) {
+
+        Database db = new Database(context, "DB1", null, 1);
+        SQLiteDatabase database = db.getWritableDatabase();
+        Cursor cursor = database.query("USER", new String[]{"NAME", "ACCOUNT_NUMBER", "PASSWORD"},
+                "NAME = ?", new String [] {nom}, null, null, null);
+        if (cursor.moveToFirst()){
+
+
+            if(cursor.getString(0).equals(nom) || cursor.getString(2).equals(pass) ){
+
+                return true;
+            }
+
+            return false;
+        }
+        database.close();
+
+        return false;
+    }
 }
