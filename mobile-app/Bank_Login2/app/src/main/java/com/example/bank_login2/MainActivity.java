63cd86d3509e960cdd776b3f1f1681ed7749911d
jusruizsa <sruizs.jr@gmail.com>
2019-06-11 06:13:44 +0200

    sendMoney

--- mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/MainActivity.java
+++ mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/MainActivity.java
@@ -3,9 +3,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+import com.example.bank_login2.dataAccess.controllers.controllerTransaction;
 import com.example.bank_login2.dataAccess.models.Account;
 import com.example.bank_login2.dataAccess.models.Customer;
+import com.example.bank_login2.dataAccess.models.Transaction;
 import com.example.bank_login2.dataAccess.repositories.UserRepository;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         UserRepository userRep = new UserRepository();
-        Account newAccount = new Account(1018484010, 400000) ;
-        Customer newCustomer = new Customer("JUAN", 1, newAccount, 123456);
-        userRep.getUSerByID(this, 1);
+        Account juanAccount = new Account(1018484010, 100000) ;
+        Account pepeAccount = new Account(1234567890, 150000);
+        Customer sender = new Customer("JUAN", 1, juanAccount, 123456);
+        Customer receiver = new Customer("PEPE", 2, pepeAccount, 123456);
+        Date date = new Date();
+        Transaction transaction = new Transaction(date, juanAccount, pepeAccount, 50000);
+        controllerTransaction send = new controllerTransaction();
+        send.sendMoney(this, transaction, juanAccount, pepeAccount);
+
     }
 
 
