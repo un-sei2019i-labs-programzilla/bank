b2a2b855045c99a718663fc29297d68cf423fb57
Nico <nsochoaga@unal.edu.co>
2019-06-11 03:43:13 -0500

    logfinal

--- mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/MainActivity.java
+++ mobile-app/Bank_Login2/app/src/main/java/com/example/bank_login2/MainActivity.java
@@ -1,36 +1,50 @@
 package com.example.bank_login2;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 
-import com.example.bank_login2.dataAccess.controllers.controllerTransaction;
 import com.example.bank_login2.dataAccess.models.Account;
 import com.example.bank_login2.dataAccess.models.Customer;
-import com.example.bank_login2.dataAccess.models.Transaction;
 import com.example.bank_login2.dataAccess.repositories.UserRepository;
 
-import java.util.Date;
-
 public class MainActivity extends AppCompatActivity {
-
+    private EditText username,password;
+    public static final String EXTRA_MESSAGE = "com.example.bank_login2.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+
         UserRepository userRep = new UserRepository();
-        Account juanAccount = new Account(1018484010, 100000) ;
-        Account pepeAccount = new Account(1234567890, 150000);
-        Customer sender = new Customer("JUAN", 1, juanAccount, 123456);
-        Customer receiver = new Customer("PEPE", 2, pepeAccount, 123456);
-        Date date = new Date();
-        Transaction transaction = new Transaction(date, juanAccount, pepeAccount, 50000);
-        controllerTransaction send = new controllerTransaction();
-        send.sendMoney(this, transaction, juanAccount, pepeAccount);
+        Account newAccount = new Account(1018484010, 400000) ;
+        Customer newCustomer = new Customer("JUAN", 1, newAccount, 123456);
+        userRep.addUser(this ,newCustomer,newAccount);
+
 
     }
 
+    public void Login(View view) {
+        String nom,pass;
+        nom = username.getText().toString();
+        pass= password.getText().toString();
+        UserRepository userRep = new UserRepository();
 
+        if ( userRep.comp(this,nom,pass)){
+            Intent intent = new Intent(this, menu.class);
+            EditText editText = (EditText) findViewById(R.id.username);
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }else{
+        }
+    }
 
 
 }
